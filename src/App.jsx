bf01f082e2afdb426e4b1116e3269c0e6974ddff
tomas-nogueira <tomasmnogueira2006@gmail.com>
2023-08-16 14:57:23 -0300

    Modelo do React pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-function App() {
+function App(props) {
   return (
     <>
     </>
   );
 }
-
 export default App;
---
+++ src/components/BoasVindas.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+function BoasVindas() {
+  
+    const[ nome, setNome ] = useState('')
+  
+    return (
+    <>
+        <div>
+            <input type="text" onChange={ (e) => {setNome( e.target.value )}}/>
+        </div>
+        <div>
+            <span>{nome}</span>
+        </div>
+    </>
+  )
+}
+
+export default BoasVindas
